865afa95501f994d3ffb26039d7db1462dbbc9a0
Omkar Phansopkar <omkarphansopkar@gmail.com>
2023-10-29 03:34:11 +0530

    Added OCR api route
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import requests
+from flask_restful import Resource, reqparse
+from flask import jsonify, request, send_file
 from flask import Flask, jsonify, request
 from flask_restful import Api
 from mongo_engine import db
@@ -28,6 +31,42 @@ db.init_app(app)
 api.add_resource(Translation, "/translation")
 
 api.add_resource(Chat, "/chat")
+
+API_KEY = os.environ.get('OCR_API_KEY')
+OCR_SPACE_ENDPOINT = 'https://api.ocr.space/parse/image'
+
+
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image provided"}), 400
+
+    image = request.files['image']
+
+    # Create a payload with your API key
+    payload = {
+        'apikey': API_KEY,
+    }
+
+    files = {
+        'image': (image.filename, image.read())
+    }
+
+    response = requests.post(OCR_SPACE_ENDPOINT, files=files, data=payload)
+
+    if response.status_code == 200:
+        result = response.json()
+        if 'ParsedResults' in result:
+            text = result['ParsedResults'][0]['ParsedText']
+
+            ## Use this text to generate
+            return jsonify({"text": text})
+        else:
+            return jsonify({"error": "OCR failed"}), 500
+    else:
+        return jsonify({"error": "OCR API request failed"}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)

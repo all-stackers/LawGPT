4b3ef5d21585d9afe734ac49f6a73f28a9b47e3d
Jhenil Parihar <jhenilparihar@gmail.com>
2023-10-29 10:46:21 +0530

    commit

--- client/app/askai/page.js
+++ client/app/askai/page.js
@@ -1,18 +1,15 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
-import { useParams } from "next/navigation";
+import { ScaleLoader } from "react-spinners";
 
 const Chat = () => {
-  const params = useParams();
-
   const [chatMessages, setChatMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState("");
+  const [askingAI, setAskingAI] = useState(false);
+
   const chatContainerRef = useRef(null);
-  const id = parseInt(params.chatId);
-  // console.log(params.chatId);
 
   useEffect(() => {
-    // Scroll to the bottom when chatMessages change
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop =
         chatContainerRef.current.scrollHeight;
@@ -26,6 +23,7 @@ const Chat = () => {
 
   const handleSendMessage = () => {
     if (inputMessage.trim() === "") return;
+    setAskingAI(true);
     const newMessage = {
       message: inputMessage,
       sender: "user",
@@ -37,27 +35,27 @@ const Chat = () => {
     myHeaders.append("Content-Type", "application/json");
 
     var raw = JSON.stringify({
-      "question": inputMessage,
+      question: inputMessage,
     });
 
     var requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: myHeaders,
       body: raw,
-      redirect: 'follow'
+      redirect: "follow",
     };
 
     fetch("http://localhost:5000/askai", requestOptions)
-      .then(response => response.json())
-      .then(result => {
+      .then((response) => response.json())
+      .then((result) => {
         const newMessage2 = {
           message: result.data,
           sender: "agent",
         };
+        setAskingAI(false);
         setChatMessages([...chatMessages, newMessage, newMessage2]);
       })
-      .catch(error => console.log('error', error));
-
+      .catch((error) => console.log("error", error));
   };
 
   return (
@@ -66,73 +64,86 @@ const Chat = () => {
         ref={chatContainerRef}
         className="flex-1 pb-[150px] bg-[#fdfdfd] overflow-y-auto"
       >
-        {chatMessages.length >0 && chatMessages.map((message, index) => (
-          <div
-            key={message.message}
-            className={` py-8 w-[100%] border border-[#e5e5e5] border-solid flex flex-row ${
-              index % 2 === 0
-                ? "bg-white text-gray-800 font-medium"
-                : "bg-[#f7f7f8] text-gray-800"
-            }`}
-          >
-            <div className="w-[750px] mx-auto flex flex-row">
-              {index % 2 === 0 ? (
-                <>
-                  <div className="w-[30px] mr-[20px] h-[30px]  text-[white]">
-                    <img src="/assets/icon/user.png" />
-                  </div>
-                  <div className="flex w-[90%] flex-row justify-between	">
-                    <div className="w-[95%]"> {message.message}</div>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className="w-[30px] mr-[20px] h-[30px] ">
-                    <img src="/assets/icon/brain.png" />
-                  </div>
-                  <div className="flex w-[90%] flex-row justify-between	">
-                    <div className="w-[95%]"> {message.message}</div>
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="grey"
-                        class="w-[18px] h-[18px]"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                        />
-                      </svg>
+        {chatMessages.length > 0 &&
+          chatMessages.map((message, index) => (
+            <div
+              key={message.message}
+              className={` py-8 w-[100%] border border-[#e5e5e5] border-solid flex flex-row ${
+                index % 2 === 0
+                  ? "bg-white text-gray-800 font-medium"
+                  : "bg-[#f7f7f8] text-gray-800"
+              }`}
+            >
+              <div className="w-[750px] mx-auto flex flex-row">
+                {index % 2 === 0 ? (
+                  <>
+                    <div className="w-[30px] mr-[20px] h-[30px]  text-[white]">
+                      <img src="/assets/icon/user.png" />
                     </div>
-                  </div>
-                </>
-              )}
+                    <div className="flex w-[90%] flex-row justify-between	">
+                      <div className="w-[95%]"> {message.message}</div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="w-[30px] mr-[20px] h-[30px] ">
+                      <img src="/assets/icon/brain.png" />
+                    </div>
+                    <div className="flex w-[90%] flex-row justify-between	">
+                      <div className="w-[95%]"> {message.message}</div>
+                      <div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="grey"
+                          class="w-[18px] h-[18px]"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       {chatMessages.length == 0 && (
         <div className="grid grid-cols-2 gap-4 w-[750px] pb-[90px]  mx-auto">
-          <div className="p-[5px] px-4 rounded-[12px] border-[1px] border-gray-300 hover:bg-[#ececf1]">
+          <div
+            className="p-[5px] px-4 rounded-[12px] border-[1px] border-gray-300 hover:bg-[#ececf1]"
+            onClick={() => {
+              setInputMessage(
+                "What are the key elements to establish a valid contract under the Indian Contract Act, 1872?"
+              );
+            }}
+          >
             <h1 className="text-[#40414f] text-[14px] font-bold">
-              Named Entity Recognition (NER)
+              What are the key elements
             </h1>
             <p className="text-[#40414f] text-[14px] opacity-[0.5] font-medium text-[12px]">
-              Identify and tag entities such as names of people, organizations,
-              dates, and locations
+              to establish a valid contract under the Indian Contract Act, 1872?
             </p>
           </div>
-          <div className="p-[5px] px-4 rounded-[12px] border-[1px] border-gray-300 hover:bg-[#ececf1]">
+          <div
+            className="p-[5px] px-4 rounded-[12px] border-[1px] border-gray-300 hover:bg-[#ececf1]"
+            onClick={() => {
+              setInputMessage(
+                "Give me a brief overview of the legal framework for trademark registration in India."
+              );
+            }}
+          >
             <h1 className="text-[#40414f] text-[14px] font-bold">
-              Automated Document Summarization
+              Give me a brief overview
             </h1>
             <p className="text-[#40414f] text-[14px] opacity-[0.5] font-medium text-[12px]">
-              Condense lengthy legal documents into concise summary to grasp the
-              main points of a case.
+              to establish a valid contract under the Indian Contract Act, 1872?
             </p>
           </div>
         </div>
@@ -150,11 +161,15 @@ const Chat = () => {
           className="bg-white text-white px-4 py-2 h-[50px] border-[1px] rounded-r-[10px] hover:bg-gray-200"
           onClick={handleSendMessage}
         >
-          <img
-            className="h-[20px]"
-            src={`/assets/icon/send.svg`}
-            alt="navicon"
-          />
+          {askingAI ? (
+            <ScaleLoader className="mt-[-4px]" color="#19c37d" />
+          ) : (
+            <img
+              className="h-[20px]"
+              src={`/assets/icon/send.svg`}
+              alt="navicon"
+            />
+          )}
         </button>
       </div>
     </div>
--- client/app/view-pdf/page.js
+++ client/app/view-pdf/page.js
@@ -1,11 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { ScaleLoader } from "react-spinners";
+
 const PDFViewer = () => {
+  const [chatMessages, setChatMessages] = useState("");
+  const [inputMessage, setInputMessage] = useState("");
+  const [askingAI, setAskingAI] = useState(false);
+
+  const onEnterPress = async (event) => {
+    if (event.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (inputMessage.trim() === "") return;
+    setChatMessages("");
+    setAskingAI(true);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      question: "Explain this legal phrase in simple term: " + inputMessage,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:5000/askai", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setChatMessages(result.data);
+        setAskingAI(false);
+      })
+      .catch((error) => console.log("error", error));
+  };
   return (
     <div className="flex flex-wrap">
-      {/* Left side (PDF Viewer) */}
       <div className="w-full md:w-7/12 p-4 h-full">
-        {/* Replace the URL with the URL of your PDF */}
         <iframe
-          src="/assets/AFFAIRE AVCIOgLU c. TÜRKiYE.pdf"
+          src="https://bafkreiccn74vexqlat2kz32i4zo75cd34ihkgaanatrppnjl6l42z46cuy.ipfs.dweb.link"
           width="100%"
           height="530"
           frameBorder="0"
@@ -25,23 +65,26 @@ const PDFViewer = () => {
         <h1 className="text-xl font-medium">Simplify Legal Phrases</h1>
         <input
           type="text"
-          name="price"
-          id="price"
           className="h-[50px] w-full rounded-[10px] border-[1px] border-gray-200 px-[30px] box-border text-[#000000] outline-none my-[20px] focus:border-blue-300 focus:border-2"
           placeholder="Enter Text Here..."
+          value={inputMessage}
+          onKeyDown={onEnterPress}
+          onChange={(e) => setInputMessage(e.target.value)}
         />
         <div
           style={{
             overflow: "auto",
             maxHeight: "230px",
           }}
-          className="bg-gray-100 h-[230px] p-4 rounded-lg shadow-md"
+          className="bg-gray-100 h-[230px] p-4 rounded-lg shadow-md flex"
         >
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing
+          {askingAI && (
+            <div className="mt-4 ml-auto mr-auto">
+              <ScaleLoader color="#7C3AED" />
+              Hold on!
+            </div>
+          )}
+          {chatMessages}
         </div>
       </div>
     </div>
